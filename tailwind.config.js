67412295e18431aaddfeb201ae4891f79bc1c771
Aktarus006 <bastinjoachim@gmail.com>
2025-12-08 15:57:45 +0100

    js

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: false,
+  theme: {
+    extend: {
+      height: {
+        94: '22rem',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
